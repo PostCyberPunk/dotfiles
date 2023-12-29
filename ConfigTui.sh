d0219f9eac34c0e14db97de132655376241a7377
PostCyberPunk <lyhguest2@outlook.com>
2023-12-29 14:42:40 +0800

    ft(cfg):stow tui

--- ConfigTui.sh
+++ ConfigTui.sh
@@ -10,3 +10,10 @@ read -p "Install LF Preview?"munix
 if [$munix=[yY]]; then
 	yay -S --needed ctpv-git chafa atool perl-image-exiftool jq glow ffmpegthumbnailer git-delta poppler glmark2
 fi
+stow -nv 1tui
+read -r "Stow your config fot tui core?:(y/N)" dostow
+if [$dostow=[yY]]; then
+	stow -v 1tui
+else
+	exit 0
+fi
