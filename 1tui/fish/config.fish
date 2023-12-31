94eaf43af7aa0f237f78f678a40d1f654a54ab80
PostCyberPunk <lyhguest2@outlook.com>
2023-12-31 17:08:45 +0800

    fix(fish): cursor;prompt icon;vivid
    add vivid
    
    fish prompt cursor
    
    hypr keybinds

--- 1tui/fish/config.fish
+++ 1tui/fish/config.fish
@@ -1,7 +1,7 @@
 set fish_greeting
 set -gx EDITOR /usr/bin/nvim
 set -gx VISUAL /usr/bin/nvim
-# alias
+########### alias###########
 alias md="mkdir"
 alias l="eza -lA --icons"
 alias n="nvim"
@@ -9,6 +9,7 @@ alias b="bat"
 alias lc="lfcd"
 alias lg="lazygit"
 alias slf="sudo -u buzz lf"
+alias t="tmux"
 # keybinds
 function fish_user_key_bindings
     bind \ce 'fish_commandline_prepend "sudo -E nvim"'
@@ -33,10 +34,30 @@ function fish_user_key_bindings
 end
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # if string match -q -- 'tmux*' $TERM
+    #     set -g fish_vi_force_cursor 1
+    # end
 end
-########### pure ########### 
+########### Prompt and Plugins ########### 
 # set pure_enable_single_line_prompt true
 # set pure_shorten_prompt_current_directory_length 1
 # set --global tide_prompt_min_cols 300
-set --global tide_prompt_transient_enabled false
+set --global tide_prompt_transient_enabled true
+# set --global tide_character_vi_icon_default 
+# set --global tide_character_vi_icon_default 
+# set --global tide_character_vi_icon_default 
+# set --global tide_character_vi_icon_default 󰻃
+set --global tide_character_vi_icon_default 
+set --global tide_character_icon 
+set fzf_preview_file_cmd fzf_pcp_previewer
+############ path ###########
 fish_add_path home/buzz/.spicetify
+########### Variables ###########
+# set LS_COLORS "$(vivid generate catppuccin-mocha)"
+########### Enviromental ###########
+export LS_COLORS="$(vivid generate catppuccin-mocha)"
+########### Fix ###########
+fish_vi_key_bindings
+set -g fish_cursor_insert line
+set -g fish_cursor_default block
+set -g fish_cursor_replace underscore
--- ConfigTui.sh
+++ ConfigTui.sh
@@ -4,11 +4,11 @@ if [[ "$EUID" = 0 ]]; then
 fi
 read -p "Install Modern Unix Collection?" munix
 if [[ $munix = [yY] ]]; then
-	yay -S --needed duf dust fd navi gping procs jq sd the_silver_searcher choose eza tldr rsync bat || echo "!!!!!Installation Failed!!!!!"
+	yay -S --needed duf dust fd navi gping procs jq sd the_silver_searcher choose eza tldr rsync bat vivid || echo "!!!!!Installation Failed!!!!!"
 fi
 read -p "Install LF Preview?" lfcp
 if [[ $lfcp = [yY] ]]; then
-	yay -S --needed ctpv-git chafa atool perl-image-exiftool jq glow ffmpegthumbnailer git-delta poppler glmark2 bat || echo "!!!!!Installation Failed!!!!!"
+	yay -S --needed ctpv-git chafa atool perl-image-exiftool jq glow ffmpegthumbnailer git-delta poppler glmark2 bat vivid || echo "!!!!!Installation Failed!!!!!"
 fi
 # rm -r ~/.config/fish
 dotinker=$(pwd)/lib/dotinker.sh
