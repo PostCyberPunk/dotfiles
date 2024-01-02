dd7a84e2ffb89ea42b4818eddbb9d8b52c8849d6
PostCyberPunk <lyhguest2@outlook.com>
2024-01-02 11:44:29 +0800

    fix(tui):add unrar

--- ConfigTui.sh
+++ ConfigTui.sh
@@ -4,7 +4,7 @@ if [[ "$EUID" = 0 ]]; then
 fi
 read -p "Install Modern Unix Collection?" munix
 if [[ $munix = [yY] ]]; then
-	yay -S --needed duf dust fd navi gping procs jq sd the_silver_searcher choose eza tldr rsync bat vivid || echo "!!!!!Installation Failed!!!!!"
+	yay -S --needed duf dust fd navi gping procs jq sd the_silver_searcher choose eza tldr rsync bat vivid unrar || echo "!!!!!Installation Failed!!!!!"
 fi
 read -p "Install LF Preview?" lfcp
 if [[ $lfcp = [yY] ]]; then
