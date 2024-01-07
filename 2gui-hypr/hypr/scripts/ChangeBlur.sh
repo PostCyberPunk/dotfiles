2756f4299d835ded338231e9b6c3a69019175c15
PostCyberPunk <lyhguest2@outlook.com>
2024-01-07 10:42:03 +0800

    fix(blur)

--- 2gui-hypr/hypr/scripts/ChangeBlur.sh
+++ 2gui-hypr/hypr/scripts/ChangeBlur.sh
@@ -5,11 +5,11 @@ notify_icon="$HOME/.config/swaync/images/bell.png"
 STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
 
 if [ "${STATE}" == "2" ]; then
-	hyprctl keyword decoration:blur:size 2
+	hyprctl keyword decoration:blur:size 5
 	hyprctl keyword decoration:blur:passes 1
   notify-send -e -u low -i "$notify_icon" "Less blur"
 else
-	hyprctl keyword decoration:blur:size 5
+	hyprctl keyword decoration:blur:size 12
 	hyprctl keyword decoration:blur:passes 2
   notify-send -e -u low -i "$notify_icon" "Normal blur"
 fi
