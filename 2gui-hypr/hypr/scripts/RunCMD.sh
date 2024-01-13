4235af77cda06c54fc6c03c1baddd4e1111d5d23
PostCyberPunk <lyhguest2@outlook.com>
2024-01-13 15:18:35 +0800

    help for runcmd

--- 2gui-hypr/hypr/scripts/RunCMD.sh
+++ 2gui-hypr/hypr/scripts/RunCMD.sh
@@ -4,6 +4,9 @@ source ~/.config/hypr/lib/ui_cmd.sh
 source ~/.config/hypr/lib/device_cmd.sh
 source ~/.config/hypr/lib/fzf_cmd.sh
 
+help() {
+  declare -F
+}
 if [[ ! -z "$1" ]]; then
 	$1 "${@:2}"
 fi
