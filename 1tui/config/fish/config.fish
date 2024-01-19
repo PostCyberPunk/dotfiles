a35e5325fe3631bc9956ec2ed3b56814e39bbcc6
PostCyberPunk <lyhguest2@outlook.com>
2024-01-19 15:26:47 +0800

    fix(fish):move to interactive

--- 1tui/config/fish/config.fish
+++ 1tui/config/fish/config.fish
@@ -1,13 +1,3 @@
-########### alias###########
-alias p="fish --private"
-alias md="mkdir"
-alias l="eza -lA --icons"
-alias n="nvim"
-alias b="bat"
-alias lc="lfcd"
-alias lg="lazygit"
-alias slf="sudo -u $USER lf"
-alias zj="zellij"
 # keybinds
 function fish_user_key_bindings
     bind \e\cv 'fish_commandline_prepend "proxychains "'
@@ -45,37 +35,48 @@ if status is-interactive
     # if string match -q -- 'tmux*' $TERM
     #     set -g fish_vi_force_cursor 1
     # end
+    ########### alias###########
+    alias p="fish --private"
+    alias md="mkdir"
+    alias l="eza -lA --icons"
+    alias n="nvim"
+    alias b="bat"
+    alias lc="lfcd"
+    alias lg="lazygit"
+    alias slf="sudo -u $USER lf"
+    alias zj="zellij"
+    ########### Prompt and Plugins ########### 
+    # set pure_enable_single_line_prompt true
+    # set pure_shorten_prompt_current_directory_length 1
+    # set --global tide_prompt_min_cols 300
+    set --global tide_prompt_transient_enabled true
+    # set --global tide_character_vi_icon_default 
+    # set --global tide_character_vi_icon_default 
+    # set --global tide_character_vi_icon_default 
+    # set --global tide_character_vi_icon_default 󰻃
+    # set --global tide_character_vi_icon_default 
+    set --global tide_character_vi_icon_default 
+    # set --global tide_character_vi_icon_default 󰁎
+    # set --global tide_character_icon 
+    set --global tide_character_icon 
+    ########### Variables ###########
+    set fish_greeting
+    ###fish fzf
+    set fzf_preview_file_cmd fzf_pcp_previewer
+    set fish_fzf_default_opts --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"
+    for temp_mode in fzf_directory_opts fzf_git_log_opts fzf_git_status_opts fzf_history_opts fzf_processes_opts fzf_variables_opts
+        set $temp_mode $fish_fzf_default_opts
+    end
+    ########### Fix ###########
+    abbr --add dotdot --regex '^\.\.+$' --function multicd
 end
-########### Prompt and Plugins ########### 
-# set pure_enable_single_line_prompt true
-# set pure_shorten_prompt_current_directory_length 1
-# set --global tide_prompt_min_cols 300
-set --global tide_prompt_transient_enabled true
-# set --global tide_character_vi_icon_default 
-# set --global tide_character_vi_icon_default 
-# set --global tide_character_vi_icon_default 
-# set --global tide_character_vi_icon_default 󰻃
-# set --global tide_character_vi_icon_default 
-set --global tide_character_vi_icon_default 
-# set --global tide_character_vi_icon_default 󰁎
-# set --global tide_character_icon 
-set --global tide_character_icon 
 ############ path ###########
 fish_add_path ~/.spicetify
 fish_add_path ~/.cargo/bin
 ########### Variables ###########
-set fish_greeting
 set -gx EDITOR /usr/bin/nvim
 set -gx VISUAL /usr/bin/nvim
 
-###fish fzf
-set fzf_preview_file_cmd fzf_pcp_previewer
-set fish_fzf_default_opts --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"
-for temp_mode in fzf_directory_opts fzf_git_log_opts fzf_git_status_opts fzf_history_opts fzf_processes_opts fzf_variables_opts
-    set $temp_mode $fish_fzf_default_opts
-end
 ########### Enviromental ###########
 export LS_COLORS="$(vivid generate catppuccin-mocha)"
 export FZF_DEFAULT_OPTS='--bind "ctrl-y:execute-silent(echo -n $(pwd)/{} | wl-copy)+abort"'
-########### Fix ###########
-abbr --add dotdot --regex '^\.\.+$' --function multicd
