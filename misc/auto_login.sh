c671aef6fa981658f0d1d8efef571a1e06bcced3
PostCyberPunk <lyhguest2@outlook.com>
2024-01-20 19:56:42 +0800

    fix(auto-login)remove from fish config,no tty1;add log

--- misc/auto_login.sh
+++ misc/auto_login.sh
@@ -1,12 +1,22 @@
-if [[ $EUID -ne 0 ]]; then 
-  echo "Failed:Run as root!"
-  exit
+if [[ $EUID -ne 0 ]]; then
+	echo "Failed:Run as root!"
+	exit
 fi
 read -p "Auto Login User: " uname
 tname='$TERM'
 mkdir /etc/systemd/system/getty@tty1.service.d
-cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
+cat >/etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin $uname %I $tname
 EOF
+_login_cfg=$(grep "is-login")
+if [[ $_login_cfg = "" ]]; then
+	tee -a >>~/.config/fish/config.fish <<EOF
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1 
+        exec Hyprland >.hyprlog.txt 2> hyprerr.txt
+    end
+end
+EOF
+fi
