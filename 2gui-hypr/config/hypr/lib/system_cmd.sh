7683f32cd3217d11f3428b0950db7706e2a2ebef
PostCyberPunk <buzzhoot@gmail.com>
2024-01-24 17:35:50 +0800

    feat(hypr): comfirm;lock touchpad;fcitx pinyin update
    confirm cmd
    
    confirm refactor
    
    format
    
    confirm add TITLE
    
    confirm on boilerpalte
    
    touchpad temp
    
    fix confirm
    
    mouse to win+z
    
    touchpad off when first keydown
    
    fix pinyin

--- 2gui-hypr/config/hypr/lib/system_cmd.sh
+++ 2gui-hypr/config/hypr/lib/system_cmd.sh
@@ -24,6 +24,36 @@ noti_c() {
 	notify-send -e -u critical "$1"
 }
 
+_confirm_rofi() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-kb-accept-entry 'Return,space' \
+		-dmenu \
+		-mesg "$1"
+	# -theme ${dir}/${theme}.rasi
+}
+_need_confirm() {
+	local _result="$(echo -e "No\nYes" | _confirm_rofi "$1")"
+	if [[ "$_result" == "Yes" ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+sys_reboot() {
+	if _need_confirm "Reboot system?"; then
+		reboot
+	fi
+}
+sys_poweroff() {
+	if _need_confirm "Shutdown system?"; then
+		poweroff
+	fi
+}
+
 reload_waybar() {
 	_ps=(waybar rofi)
 	for _prs in "${_ps[@]}"; do
@@ -37,20 +67,19 @@ reload_waybar() {
 	if [[ -z $1 ]]; then
 		waybar &
 	fi
-  noti_n "Reload Waybar"
+	noti_n "Reload Waybar"
 }
-update_waybar()
-{
+update_waybar() {
 	pkill -RTMIN+4 waybar
-  noti_n "Refresh Waybar"
+	noti_n "Refresh Waybar"
 }
-reload_hypr(){
-  hyprctl reload
-  noti_n "Reload Hyprland"
+reload_hypr() {
+	hyprctl reload
+	noti_n "Reload Hyprland"
 }
-reload_all(){
-  reload_hypr
-  reload_waybar
+reload_all() {
+	reload_hypr
+	reload_waybar
 }
 
 restart_waybar_if_needed() {
--- 2gui-hypr/config/hypr/scripts/Xmenu.sh
+++ 2gui-hypr/config/hypr/scripts/Xmenu.sh
@@ -5,8 +5,7 @@ iDIR="$HOME/.config/swaync/icons"
 RunCMD="$HOME/.config/hypr/scripts/RunCMD.sh"
 scriptsDir="$HOME/.config/hypr/scripts"
 rofiDir="$HOME/.config/rofi/scripts"
-yes='Yes'
-no='No'
+
 # Note: You can add more options below with the following format:
 # ["TITLE"]="link"
 
@@ -21,7 +20,7 @@ declare -A menu_options=(
 	["wbl WaybarLayout"]="$rofiDir/WaybarLayout.sh"
 	["wbr Reload Waybar"]="$RunCMD reload_waybar"
 	["wbu Update Waybar"]="$RunCMD update_waybar"
-  #hypr
+	#hypr
 	["hpr Reload hyprland"]="$RunCMD reload_hypr"
 	["rld Reload All"]="$RunCMD reload_all"
 	["gm GameMode"]="$RunCMD toggle_gamemode"
@@ -29,68 +28,39 @@ declare -A menu_options=(
 	["flt Float all window"]="hyprctl dispatch workspaceopt allfloat"
 	["wop1 enable_opaque"]="$RunCMD enable_opaque"
 	["wop0 disable_opaque"]="$RunCMD disable_opaque"
-  #rofi
+	#rofi
 	["; Launcher"]="rofi -show drun -theme $HOME/.config/rofi/launchers/launcher.rasi"
 	["rd RofiBeats"]="$rofiDir/RofiBeats.sh"
 	["emj RofiEmoji"]="$rofiDir/RofiEmoji.sh"
 	["cc Calculator"]="rofi -modi \"calc:$rofiDir/rofi-calc.sh\" -show calc -theme $HOME/.config/rofi/config-long.rasi"
 	["fd Finder"]="rofi -modi \"find:$rofiDir/finder.sh\" -show find -theme $HOME/.config/rofi/config-long.rasi"
 	["tr Translation"]="fish -c rofi_trans -theme $HOME/.config/rofi/config-long.rasi"
-  #System
-	["QQ Shutdown"]="needConfim "poweroff""
-	["RR Reboot"]="needConfim "reboot""
+	#System
+	["QQ Shutdown"]="$RunCMD sys_poweroff"
+	["RR Reboot"]="$RunCMD sys_reboot"
 	["wl Wlogout"]="$scriptsDir/System/Wlogout.sh"
 	["ml MonitorLayout"]="$scriptsDir/System/MonitorLayout.sh"
 	["gpu GPU Switcher"]="$scriptsDir/System/GPU.sh"
 	["cn Close Notifactions"]="swaync-client -C"
 	["DM0"]="$RunCMD disable_edp1"
 	["DM1"]="$RunCMD enable_edp1"
-  #App
+	#App
 	["ff firefox"]="firefox"
 	["vb VitrualBox"]="virtualbox"
 	["vm VitrualBox"]="vboxmanage startvm Larch"
 	["v2 v2raya"]="firefox http://localhost:2017/"
 	["sp Spotify"]="spotify"
 	["lg Lazygit"]="kitty --class flkt3lg lazygit"
-  ["gL GitLogin"]="$RunCMD startGitLogin"
-  ["bp1 center template"]="$RunCMD startCenter"
-  ["td todo notes"]="$RunCMD open_notes"
-  #Utils
+	["gL GitLogin"]="$RunCMD startGitLogin"
+	["bp1 center template"]="$RunCMD startCenter"
+	["td todo notes"]="$RunCMD open_notes"
+	#Utils
 	["mnt MountDisk"]="$rofiDir/rofi-usb-mount.sh"
 	["cp PickColor(RGB)"]="hyprpicker -f rgb -a"
 	["cph PickColor(hex)"]="hyprpicker -f hex -a"
 
 )
 
-# Function for displaying notifications
-notification() {
-	notify-send -e -u low "$@"
-}
-
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-kb-accept-entry 'Return,space' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?'
-	# -theme ${dir}/${theme}.rasi
-}
-
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$no\n$yes" | confirm_cmd
-}
-needConfim() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		$1
-	fi
-}
-# myt="$(notification number)"
 # Main function
 main() {
 	choice=$(
@@ -108,8 +78,6 @@ main() {
 
 	link="${menu_options[$choice]}"
 	$link
-	# notification "$choice"
-	# test_cmd
 }
 
 main
