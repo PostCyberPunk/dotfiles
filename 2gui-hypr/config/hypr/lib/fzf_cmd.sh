b3311e4003563a2ff9ed820579fb3c270043d0db
PostCyberPunk <lyhguest2@outlook.com>
2024-03-06 14:59:06 +0800

    add linker_fzf

--- 2gui-hypr/config/hypr/lib/fzf_cmd.sh
+++ 2gui-hypr/config/hypr/lib/fzf_cmd.sh
@@ -42,3 +42,42 @@ translate_shell() {
 	fi
 	kitty --class "flkt5" -o font_size=16 fish -C "trans $1 \"$transtext\"|fzf --ansi --reverse --multi|wl-copy"
 }
+
+linker_fzf() {
+	# use fzf create a symbolic link from source_file  to selected file in target directory
+	source_dir="${1%/}"
+	target_file="$2"
+	usage() {
+		echo "Usage: linker.sh [source_dir] [target_file] "
+	}
+	# check if target directory exists
+	if [ ! -d $source_dir ] || [ -z $source_dir ]; then
+		echo "Target directory does not exist"
+		usage
+		exit 1
+	fi
+	# check if source file is null
+	if [ -z $target_file ]; then
+		echo "target file is null"
+		usage
+		exit 1
+	fi
+	# use fzf to select a file from target directory
+	file_selected="$(find $source_dir -type f -printf "%f\n" | fzf --ansi --reverse --preview "bat --color=always --style=header,numbers $source_dir/{}" \
+		--preview-window "wrap" --bind ctrl-y:preview-up,ctrl-e:preview-down,ctrl-b:preview-page-up,ctrl-f:preview-page-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down)"
+
+	if [ -z $file_selected ]; then
+		echo "No file selected"
+		exit 1
+	fi
+	source_file="$source_dir/$file_selected"
+	# echo $source_file
+	ln -sf "$source_file" "$target_file"
+	if [ $? -eq 0 ]; then
+		echo "Link created $source_file -> $target_file"
+		notify-send $file_selected
+	else
+		echo "Link creation failed"
+		notify-send "Link create failed"
+	fi
+}
