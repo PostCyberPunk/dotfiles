9c8a96f102984d5c599e63d8d0d1c12d6ce117da
PostCyberPunk <lyhguest2@outlook.com>
2024-03-06 22:47:16 +0800

    fix install wrong pkg name

--- Install/NvimConfig.sh
+++ Install/NvimConfig.sh
@@ -2,7 +2,7 @@ if [[ "$EUID" = 0 ]]; then
 	echo "Dont Run this in sudo"
 	exit 1
 fi
-yay -S --noconfirm --needed nvim gcc fzf fd ripgrep lazygit sudo nerd-fonts-profont || echo "!!!!!Installation Failed!!!!!"
+yay -S --noconfirm --needed neovim gcc fzf fd ripgrep lazygit sudo nerd-fonts-profont || echo "!!!!!Installation Failed!!!!!"
 git clone https://github.com/postcypunk/Lazyvim ~/.config/nvim
 
 # install
