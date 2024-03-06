3bb8e275034ae4a7bdebebb7983f3d888efeba9b
PostCyberPunk <lyhguest2@outlook.com>
2024-03-06 15:10:44 +0800

    fix(install) nvim extra

--- Install/NvimConfig.sh
+++ Install/NvimConfig.sh
@@ -4,3 +4,7 @@ if [[ "$EUID" = 0 ]]; then
 fi
 yay -S --noconfirm --needed nvim gcc fzf fd ripgrep lazygit sudo nerd-fonts-profont || echo "!!!!!Installation Failed!!!!!"
 git clone https://github.com/postcypunk/Lazyvim ~/.config/nvim
+
+# install
+
+cp ~/.config/nvim/lua/pcp/extra ~/.config/nvim/lua/pcp/extra.lua
