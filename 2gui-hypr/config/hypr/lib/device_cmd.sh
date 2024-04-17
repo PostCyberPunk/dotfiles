233b22c7a891c7ae06167869a8930c838e7d6c31
PostCyberPunk <lyhguest2@outlook.com>
2024-04-17 10:39:06 +0800

    Revert "!fix:touchpad toggle"
    This reverts commit 3c164acdf727498399af04a91fa094965d280739.

--- 2gui-hypr/config/hypr/lib/device_cmd.sh
+++ 2gui-hypr/config/hypr/lib/device_cmd.sh
@@ -4,15 +4,13 @@ source ~/.config/hypr/lib/system_cmd.sh
 
 enable_touchpad_s() {
 	set_var touchpad "0"
-	# hyprctl keyword "device:$touchpad_id:enabled" true
-	hyprctl -r keyword '$touchpad_enabled' true
+	hyprctl keyword "device:$touchpad_id:enabled" true
 	pkill -RTMIN+3 waybar
 }
 
 disable_touchpad_s() {
 	set_var touchpad "1"
-	# hyprctl keyword "device:$touchpad_id:enabled" false
-	hyprctl -r keyword '$touchpad_enabled' false
+	hyprctl keyword "device:$touchpad_id:enabled" false
 	pkill -RTMIN+3 waybar
 }
 
