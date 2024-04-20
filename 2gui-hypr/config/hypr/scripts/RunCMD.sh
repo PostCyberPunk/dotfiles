3cefdf45e7ee41e1c4f4af95aa0428b1f5ca9580
PostCyberPunk <lyhguest2@outlook.com>
2024-04-20 12:08:14 +0800

    feat cmd:help

--- 2gui-hypr/config/hypr/scripts/RunCMD.sh
+++ 2gui-hypr/config/hypr/scripts/RunCMD.sh
@@ -5,7 +5,7 @@ source ~/.config/hypr/lib/device_cmd.sh
 source ~/.config/hypr/lib/fzf_cmd.sh
 
 help() {
-  declare -F
+	declare -F | awk '{print $3}' | grep -v '^_' | tr '\n' '\t' | fold -sw $(tput cols)
 }
 if [[ ! -z "$1" ]]; then
 	$1 "${@:2}"
