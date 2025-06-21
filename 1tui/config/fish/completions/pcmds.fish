bbf470308f15eb9139054ba1b9e7bac5072272d3
PostCyberPunk <lyhguest2@outlook.com>
2025-06-22 05:44:44 +0800

    fix:theme

--- 1tui/config/fish/completions/pcmds.fish
+++ 1tui/config/fish/completions/pcmds.fish
@@ -6,6 +6,12 @@ function _pcmds_subcommands
     pcmds -hs $argv[2]
 end
 
+function _pcmds_list_themes
+    pcmds theme list_themes
+end
+
 complete -f pcmds -n __fish_use_subcommand -a '(_pcmds_commands)'
 
 complete -f pcmds -n '__fish_seen_subcommand_from (_pcmds_commands)' -a '(_pcmds_subcommands (commandline -opc))'
+
+# complete -f pcmds -n '__fish_seen_argument set' -a '(_pcmds_list_themes)'
