eead2595ba2bdb805b625b43ed25a47f34897c50
PostCyberPunk <lyhguest2@outlook.com>
2025-09-20 00:53:12 +0800

    feat:git local generator

--- Install/init_git.sh
+++ Install/init_git.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
 _mail=$(gum input --header "Email address for git")
 _user=$(gum input --header "User name for git")
-git config --global user.name "$_user"
-git config --global user.email "$_mail"
+cat <<EOL >~/.config/git/local.test
+[user]
+email = $_mail
+name = $_user
+[credential "https://github.com"]
+helper =
+helper = !/run/current-system/sw/bin/gh auth git-credential
+[credential "https://gist.github.com"]
+helper =
+helper = !/run/current-system/sw/bin/gh auth git-credential
+[url "https://www.github.com/$_user/"]
+insteadOf = "ghp:"
+[url "git@github.com:$_user/"]
+insteadOf = "ghsp:"
+EOL
